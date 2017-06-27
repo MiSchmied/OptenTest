fa3d8cca1a50a576bf462d1508e77be92ec58000
MSCHMIEDB5\Michel <mschmied@byte5.de>
2017-06-27 11:34:10 +0200

    Projektdateien hinzufügen.

---
+++ OptenLocalizeTest/App_Plugins/OPTEN.Localization.Migration/opten.localization.migration.controller.js
@@ -0,0 +1,69 @@
+﻿; (function () {
+	'use strict';
+
+	angular.module("umbraco.resources").controller("OPTEN.Backoffice.Localization.Migration.Controller", localizationMigrationController)
+
+	localizationMigrationController.$inject = ["$scope", "notificationsService", "OPTEN.Backoffice.Localization.Migration.Resource", '$location', '$anchorScroll'];
+
+	function localizationMigrationController($scope, notificationsService, localizationMigrationResource, $location, $anchorScroll) {
+		$scope.loading = true;
+		$scope.selectedItem = {};
+
+		$scope.loadData = function () {
+			localizationMigrationResource.getContentTypes().then(function (data) {
+				$scope.model = data;
+				$scope.selectedItem = $scope.model[0];
+				$scope.loading = false;
+			});
+		}
+
+		$scope.save = function (data) {
+			var properties = [];
+			data.forEach(function (item, index) {
+				item.properties.forEach(function (property, index) {
+					if (property.localize) {
+						properties.push(item.name + ": " + property.alias);
+					}
+				});
+			});
+
+			if (properties.length && confirm("Are you sure you want to migrate: \n" + properties.join('\n') + "?")) {
+				$scope.loading = true;
+				localizationMigrationResource.postContentTypes(data).then(function () {
+					$scope.loadData();
+				});
+			}
+		}
+
+		$scope.migrateAllContentTypes = function () {
+			if (confirm("Are you sure you want to migrate ALL properties?")) {
+				$scope.loading = true;
+				localizationMigrationResource.migrateAllContentTypes().then(function () {
+					$scope.loadData();
+				});
+			}
+		}
+
+		$scope.goToDocType = function (target) {
+			scrollToHash(target.alias);
+			var el = document.getElementById(target.alias);
+			var siblings = document.getElementsByClassName('content-type');
+
+			for (var i = 0; i < siblings.length; i++) {
+				siblings[i].classList.remove('-scroll-target');
+			}
+
+			el.classList.add('-scroll-target');
+		}
+
+		function scrollToHash(hash) {
+			var old = $location.hash();
+			$location.hash(hash);
+			$anchorScroll();
+			$location.hash(old);
+		}
+
+		$scope.loadData();
+	};
+
+}());
---
+++ OptenLocalizeTest/App_Plugins/OPTEN.Localization.Migration/opten.localization.migration.resource.js
@@ -0,0 +1,58 @@
+﻿; (function () {
+	'use strict';
+
+	angular.module("umbraco.resources").factory("OPTEN.Backoffice.Localization.Migration.Resource", localizationMigrationResource);
+
+	localizationMigrationResource.$inject = ["$http", "umbRequestHelper"];
+
+	function localizationMigrationResource($http, umbRequestHelper) {
+		var resource = {
+			getContentTypes: getContentTypes,
+			postContentTypes: postContentTypes,
+			migrateAllContentTypes: migrateAllContentTypes,
+			postProperty: postProperty
+		};
+
+		return resource;
+
+
+		// Private functions
+
+		function getContentTypes() {
+			return umbRequestHelper.resourcePromise(
+				$http.get("/umbraco/backoffice/OPTEN/MigrationApi/GetAllContentTypes"),
+				"Failed to retrieve content types"
+			);
+		};
+
+		function postContentTypes(contentTypes) {
+			return umbRequestHelper.resourcePromise(
+				 $http({
+				 	url: "/umbraco/backoffice/OPTEN/MigrationApi/PostLocalize",
+				 	method: "POST",
+				 	data: contentTypes
+				 }),
+				"Failed to migrate content types"
+			);
+		};
+
+		function migrateAllContentTypes() {
+			return umbRequestHelper.resourcePromise(
+				$http.post("/umbraco/backoffice/OPTEN/MigrationApi/PostLocalizeAll"),
+				"Failed to migrate content types"
+			);
+		}
+
+		function postProperty(property) {
+			return umbRequestHelper.resourcePromise(
+				$http({
+					url: "/umbraco/backoffice/OPTEN/MigrationApi/PostProperty",
+					method: "POST",
+					data: property
+				}),
+				"Failed to migrate content types"
+			);
+		};
+	};
+
+}());
